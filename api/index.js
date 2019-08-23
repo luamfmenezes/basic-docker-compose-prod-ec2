18c83c882fbff217c611aa22eb3101fe5814f28f
Luam Fernandes de Menezes <luamfmenezes@gmail.com>
2019-08-23 17:34:55 -0300

    create todo aplication

--- api/index.js
+++ api/index.js
@@ -1,44 +1,56 @@
 const express = require('express');
-//const mongoose = require('mongoose');
-//const Todos = require('./src/models/Todos');
+const mongoose = require('mongoose');
+const Todos = require('./src/models/Todos');
+const cors = require('cors');
 
-const PORT = 3000;
+const PORT = 3001;
 const HOST = '0.0.0.0';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // it's mongo insted localhost 
 // becase mongo is the name of container
-// mongoose.connect(
-//     'mongodb://mongo:27017/docker-node-mongo',
-//     { useNewUrlParser : true },
-//     () => console.log('Mongoose connect sucessfull')
-// )
+mongoose.connect(
+    'mongodb://mongo:27017/docker-node-mongo',
+    { useNewUrlParser : true },
+    () => console.log('Mongoose connect sucessfull')
+)
 
 app.get('/', async (req,res) => {
-    res.send('Container online  9!');
+    res.send('Container online  5!');
 })
 
-// app.get('/todos', async (req,res) => {
-//     try{
-//         const todos = await Todos.find();
-//         res.status(200).json(todos);
-//     }catch(err){
-//         res.status(404).json(err);
-//     }
-// })
+app.get('/todos', async (req,res) => {
+    try{
+        const todos = await Todos.find();
+        res.status(200).json(todos);
+    }catch(err){
+        res.status(404).json(err);
+    }
+})
 
-// app.post('/todos', async (req,res) => {
-//     try{
-//         const { body } = req;
-//         console.log(body);
-//         const createdTodo = await Todos.create(body);
-//         res.status(200).json(createdTodo);
-//     }catch(err){
-//         res.status(404).json(err);
-//     }
-// });
+app.post('/todos', async (req,res) => {
+    try{
+        const { body } = req;
+        console.log(body);
+        const createdTodo = await Todos.create(body);
+        res.status(200).json(createdTodo);
+    }catch(err){
+        res.status(404).json(err);
+    }
+});
+
+app.delete('/todos/:_id', async (req,res) => {
+    try{
+        const { params: { _id } } = req;
+        const deletedTodo = await Todos.findOneAndDelete({_id});
+        res.status(200).json(deletedTodo);
+    }catch(err){
+        res.status(404).json(err);
+    }
+});
 
 app.listen(PORT, HOST);
--- app/src/App.js
+++ app/src/App.js
@@ -1,15 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [todos,setTodos] = useState([]);
+  const [content,setContent] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('http://localhost:3001/todos');
+      const jsonResp = await response.json();
+      setTodos(jsonResp);
+    }
+    fetchData();
+  },[]);
+
+  const addTodo = async () => {
+    const response = await fetch('http://localhost:3001/todos',{
+      method:'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body:JSON.stringify({content}),
+      mode: 'cors',
+      cache: 'default'
+    });
+    const newTodo = await response.json();
+    setTodos([...todos,newTodo]);
+  }
+
+  const deleteTodo = async (id) => {
+    const response = await fetch('http://localhost:3001/todos/'+id,{
+      method:'DELETE',
+      mode: 'cors',
+      cache: 'default'
+    });
+    const deletedTodo = await response.json();
+    const newTodos = todos.filter(el => el._id !== deletedTodo._id);
+    setTodos(newTodos);
+  }
+  const handleInput = event => setContent(event.target.value);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Lets test
-        </p>
+        <h1> Todo List </h1>
+        <div className="container-todo">
+          {
+            todos.map(el => 
+            <div className="todo" key={el._id}>
+              <p>{el.content}</p>
+              <button onClick={() => deleteTodo(el._id)}> delete </button> 
+            </div>)
+          }
+        </div>
+        <div>
+          <p>Add Todo: </p>
+          <input onChange={handleInput}/>
+          <button
+            onClick={addTodo}
+          >Add</button>
+        </div>
       </header>
     </div>
   );
